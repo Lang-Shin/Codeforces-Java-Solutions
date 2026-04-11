1d7cba3355607c106eec3a429a6593089a13d5f6
Lang-Shin <bajuracharles@gmail.com>
2026-04-11 11:40:44 +0800

    405A(Gravity Flip) - FINISHED

---
+++ ELO-900/GravityFlip.java
@@ -0,0 +1,32 @@
+// 405A - Gravity Flip
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class GravityFlip {
+
+    static int[] gravityFlip(int[] a) {
+        Arrays.sort(a);
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int[] a = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(gravityFlip(a)[i] + " ");
+        }
+
+        s.close();
+
+    }
+}
