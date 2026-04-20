0f244e5b26b7dccd65d324f38dcc9b92c845c9af
Lang-Shin <bajuracharles@gmail.com>
2026-04-20 21:42:52 +0800

    1A(Theatre Square) - FINISHED

--- ELO-1000/TheatreSquare.java
+++ ELO-1000/TheatreSquare.java
@@ -1,18 +1,25 @@
+// 1A - Theatre Square
+
 import java.util.Scanner;
 
 public class TheatreSquare{
 
-    static int numOfFlagStones(int n, int m, int a) {
-        return a;
+    static long numOfFlagStones(double n, double m, double a) {
+        
+        double width = Math.ceil(n/a);
+        double height = Math.ceil(m/a);
+
+        return (long) (width*height);
+
     }
 
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
 
-        int n = s.nextInt();
-        int m = s.nextInt();
-        int a = s.nextInt();
+        double n = s.nextDouble();
+        double m = s.nextDouble();
+        double a = s.nextDouble();
 
         System.out.println(numOfFlagStones(n, m, a));
 
