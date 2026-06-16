c40799aa7968dad68e3b33c0d69d2b5823916db2
Lang-Shin <bajuracharles@gmail.com>
2026-06-16 10:53:15 +0800

    141A - Amusing Joke --> FINISHED

--- ELO-800/AmusingJoke.java
+++ ELO-800/AmusingJoke.java
@@ -1,9 +1,41 @@
+// 141A - Amusing Joke
 
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class AmusingJoke {
+
+    static boolean pileLetter(String guess, String host, String pile) {
+
+        char[] pileArr = pile.toCharArray();
+        Arrays.sort(pileArr);
+        String newPile = new String(pileArr);
+
+        String combine = guess + host;
+        char[] combineArr = combine.toCharArray();
+        Arrays.sort(combineArr);
+        String newCombine = new String(combineArr);
+
+        if(newCombine.equals(newPile)) return true;
+
+        return false;
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Scanner s = new Scanner(System.in);
+
+        String guess = s.nextLine();
+        String host = s.nextLine();
+        String pile = s.nextLine();
+
+        boolean prank = pileLetter(guess, host, pile);
+
+        if(prank) System.out.println("YES");
+        else System.out.println("NO");
+
+        s.close();
 
     }
 }
