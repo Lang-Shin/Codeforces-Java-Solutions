9e6f74082e96b80306fda643eabc4645a9cf51fd
Lang-Shin <bajuracharles@gmail.com>
2026-06-19 11:10:39 +0800

    done

--- ELO-800/ILoveUser.java
+++ ELO-800/ILoveUser.java
@@ -7,24 +7,23 @@ public class ILoveUser {
     static int amazingScore(int n, int[] points) {
 
         int amazingTen = 0;
-
-        int score = 0;
-        int health = 2;
+    
+        int best = points[0], worst = points[0];
 
         for(int i = 1; i < points.length; i++) {
 
-            int cScore = points[i];
-
-            if(cScore > score) {
-
-                if(health < 1) {
-                    amazingTen++;
-                    health = 2;
-                }
+            if(points[i] > best) {
 
                 amazingTen++;
-                score = cScore;
-            } else if(cScore < score) health--;
+                best = points[i];
+
+            } else if(points[i] < worst) {
+
+                amazingTen++;
+                worst = points[i];
+
+            }
+
         }
 
         return amazingTen;
