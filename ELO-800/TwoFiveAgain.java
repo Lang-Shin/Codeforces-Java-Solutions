8a362570025d6c298cbd751f2c948274df33d92a
Lang-Shin <bajuracharles@gmail.com>
2026-06-19 10:27:11 +0800

    630A - Again Twenty Five! --> FINISHED

---
+++ ELO-800/TwoFiveAgain.java
@@ -0,0 +1,17 @@
+// 630A - Again Twenty Five!
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TwoFiveAgain {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.readLine();
+
+        System.out.println(25);
+
+    }
+}
