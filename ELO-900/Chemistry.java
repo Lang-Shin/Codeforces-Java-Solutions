280a306942b84d90ee0cf0b7ae0ecf621a0d8188
Lang-Shin <bajuracharles@gmail.com>
2026-06-20 10:29:23 +0800

    done

--- ELO-900/Chemistry.java
+++ ELO-900/Chemistry.java
@@ -2,33 +2,41 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Chemistry{
 
-    // static boolean chem(int n, int k, String s) {
+    static StringBuilder isPalindrome(int t, int n, int k, String s) {
 
+        StringBuilder sb = new StringBuilder();
+        int[] freq = new int[26];
 
+        for(char c : s.toCharArray()) freq[c - 'a']++;
 
-    // }
+        int odd = 0;
+        for(int f : freq) if(f%2 != 0) odd++;
+
+        if(odd <= 1) sb.append("YES");
+        else sb.append(k >= odd - 1? "YES" : "NO");
+
+        return sb;
+
+    }
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(br.readLine().trim());
+        int t = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < t; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
 
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
 
             String s = br.readLine().trim();
 
-            if(s.length() != n) System.out.println("Fucking Error"); break;
+            System.out.println(isPalindrome(t, n, k, s));
         }
-
-
 
     }
 }
