325718dfbe7b6d028f402ec0153ff4921ebf6f0a
Charles <bajuracharles@gmail.com>
2026-06-29 10:46:16 +0800

    1475B - Init Done

---
+++ ELO-900/NYNumber.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NYNumber {
+
+    static String[] wonderfulMoment(int[] years) {
+
+        String[] str = new String[years.length];
+
+        for(int i = 0; i < years.length; i++) {
+
+            if(years[i] < 2020) {
+                str[i] = "No"; 
+                continue;
+            }
+
+            if((years[i]%2020) < 2000) {
+                str[i] = "Yes"; 
+                continue;
+            }
+            else str[i] = "No";
+
+        }
+
+        return str;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        int[] years = new int[t];
+        for(int i = 0; i < t; i++) years[i] = Integer.parseInt(br.readLine());
+
+        String[] outputs = wonderfulMoment(years);
+
+        for(int i = 0; i < outputs.length; i++) System.out.println(outputs[i]);
+
+    }
+}
