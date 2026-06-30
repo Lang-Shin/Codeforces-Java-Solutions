9ee0947f9fcfeafc4708480f5714b2258e7fb0d5
Charles <bajuracharles@gmail.com>
2026-06-30 21:31:00 +0800

    1692A - Init Done

---
+++ ELO-800/Marathon.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Marathon {
+
+    static int frontOfA(int a, int b, int c, int d) {
+
+        int count = 0;
+
+        if(a < b) count++;
+        if(a < c) count++;
+        if(a < d) count++;
+
+        return count;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            System.out.println(frontOfA(a, b, c, d));
+        }
+    }
+}
