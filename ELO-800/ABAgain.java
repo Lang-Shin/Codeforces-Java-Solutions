5d842c70480eb99fc13f2a0a9ae5f87642e868d7
Charles <bajuracharles@gmail.com>
2026-07-07 11:15:15 +0800

    1999A - Init Done

---
+++ ELO-800/ABAgain.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ABAgain {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < t; i++) {
+
+            int n = Integer.parseInt(br.readLine());
+            int lastDigit = n%10;
+            n /= 10;
+
+            sb.append((n+lastDigit) + "\n");
+
+        }
+        System.out.println(sb);
+    }
+}
