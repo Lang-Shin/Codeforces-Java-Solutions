35a765732b38ceebfb13c2b3bff554b906f1b8c7
Charles <bajuracharles@gmail.com>
2026-07-07 11:59:12 +0800

    1373B - Init Done

---
+++ ELO-900/GameZO.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GameZO {
+
+    static String winner(String bit) {
+
+        int moves = bit.length() / 2;
+
+        return moves%2 == 0 ? "NET" : "DA";
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < t; i++) {
+            String bit = br.readLine();
+
+            sb.append(winner(bit) + "\n");
+        }
+
+        System.out.println(sb);
+
+    }   
+}
