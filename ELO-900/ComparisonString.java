3fbb222d0c8b26a2bb5818d44dce0bb170506713
Charles <bajuracharles@gmail.com>
2026-07-19 21:49:47 +0800

    1837B - Init Done

--- ELO-900/ComparisonString.java
+++ ELO-900/ComparisonString.java
@@ -3,26 +3,20 @@ import java.io.InputStreamReader;
 
 public class ComparisonString {
 
-    static int totalCost(String s) {
+    static int totalCost(int n, String s) {
 
         char[] operator = s.toCharArray();
         int peak = 1;
         int cumsum = 1 ;
         
-        for(int i = 0; i < operator.length-1; i++) {
-            System.out.println("\n\nIteration : " + (i+1));
+        for(int i = 0; i < n-1; i++) {
             if(operator[i] == operator[i+1]){
                 cumsum++;
                 if(cumsum > peak) peak = cumsum;
-                
-                System.out.println("\nCumsum : " + cumsum);
-                System.out.println("Peak : " + peak);
             }
             else cumsum=1;
 
         }
-
-        System.out.println("\nPeak : " + peak);
         return peak+1;
 
     }
@@ -38,7 +32,7 @@ public class ComparisonString {
             int n = Integer.parseInt(br.readLine());
             String s = br.readLine();
 
-            sb.append(totalCost(s) + "\n");
+            sb.append(totalCost(n, s) + "\n");
         }
 
         System.out.println(sb);
