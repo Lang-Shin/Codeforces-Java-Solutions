1349fbf872fb16a8ff156b94b66d47488c540588
Charles <bajuracharles@gmail.com>
2026-07-19 20:26:36 +0800

    1829B - Init Done

---
+++ ELO-800/BlankSpace.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BlankSpace {
+
+    static int consecutiveZero(int[] bits) {
+
+        int cumsum = 0;
+        int largest = 0;
+
+        for(int i = 0; i < bits.length; i++) {
+            if(bits[i] == 0) {
+                cumsum++;
+                if(cumsum > largest) largest = cumsum;
+            }
+            else cumsum = 0;
+        }
+
+        return largest;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] bits = new int[n];
+
+            for(int j = 0; j < n; j++) bits[j] = Integer.parseInt(st.nextToken());
+            sb.append(consecutiveZero(bits) + "\n");
+        }
+
+        System.out.println("\n\n" + sb);
+
+    } 
+}
