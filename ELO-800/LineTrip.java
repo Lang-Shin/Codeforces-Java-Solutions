e1c4d04ff57c69d3b5cba0d80bee62ac51f34d0f
Charles <bajuracharles@gmail.com>
2026-08-12 15:54:23 +0800

    Line Trip - Init Done

--- ELO-800/LineTrip.java
+++ ELO-800/LineTrip.java
@@ -6,14 +6,22 @@ import java.util.StringTokenizer;
 public class LineTrip {
 
     static int minVol(int[] a, int x) {
+
         int n = a.length;
+        int gap;
+        int stretched;
 
-        if(a.length <= 1) return a[0];
+        if(n <= 1) {
+            gap = a[n-1];
+            stretched = 2 * (x-gap);
 
-        int point = a[n-1] - a[n-2];
-        int max = x-point;
+            return stretched>=gap ? stretched : gap;
+        }
 
-        return max;
+        gap = a[n-1] - a[n-2];
+        stretched = 2*(x-a[n-1]);
+
+        return stretched>=gap ? stretched : gap;
     }
 
     public static void main(String[] args) throws Exception {
@@ -35,7 +43,7 @@ public class LineTrip {
             sb.append(minVol(a, x) + "\n");
         }
 
-        System.out.println("\n\n" + sb);
+        System.out.println(sb);
 
     }
 }
