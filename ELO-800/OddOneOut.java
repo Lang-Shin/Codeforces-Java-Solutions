1a95c0745368a7b56efa8af5016fb64c8843f4be
Charles <bajuracharles@gmail.com>
2026-08-19 15:21:51 +0800

    Odd One Out -- Init Done

---
+++ ELO-800/OddOneOut.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class OddOneOut{
+
+    static int differInt(int a, int b, int c) {
+
+        if(a == b) return c;
+        else if(a == c) return b;
+        else return a;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            sb.append(differInt(a, b, c) + "\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
