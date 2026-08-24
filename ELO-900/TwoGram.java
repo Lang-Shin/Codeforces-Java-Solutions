c1aea5ccdb0e45aaac92898cf389f59661a72a54
Charles <bajuracharles@gmail.com>
2026-08-24 08:12:12 +0800

    Two-gram -- Init Done

---
+++ ELO-900/TwoGram.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TwoGram{
+
+    static String subStrMaxing(int n, String s) {
+        int[][] comb = new int[26][26];
+
+        for(int i = 0; i < n-1; i++) {
+            int first = s.charAt(i) - 'A';
+            int second = s.charAt(i+1) - 'A';
+            comb[first][second]++; 
+        }
+
+        int maxCount = -1;
+        int bestFirst = 0;
+        int bestSecond = 0;
+
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < 26; j++) {
+                if(comb[i][j] > maxCount) {
+                    maxCount = comb[i][j];
+                    bestFirst = i;
+                    bestSecond = j;
+                }
+            }
+        }
+
+        char c1 = (char) (bestFirst + 'A');
+        char c2 = (char) (bestSecond + 'A');
+
+        return "" + c1 + c2;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        System.out.println(subStrMaxing(n, s));
+
+    }
+}
