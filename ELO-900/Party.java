cb3c2f01628d1302bf00dffd3b7a94924f2b99fb
Charles <bajuracharles@gmail.com>
2026-09-02 08:49:49 +0800

    Minimize! -- Init Done

---
+++ ELO-900/Party.java
@@ -0,0 +1,17 @@
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+
+// public class Party {
+
+//     static 
+
+//     public static void main(String[] args) throws Exception {
+
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int n = Integer.parseInt(br.readLine());
+
+//         int[] emp = new int[n];
+//         for(int i = 0; i < n; i++) emp[i] = Integer.parseInt(br.readLine());
+
+//     }
+// }
